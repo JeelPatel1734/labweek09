96167683b83ee6e10489273f621144ef96363cea
Jeel Prakashchandra Patel <101410368@georgebrown.ca>
2024-11-03 18:33:51 -0500

    labweek09

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const studentInfo = {
+    course: "Welcome to Fullstack Development - I",
+    labTitle: "React JS Programming Week09 Lab exercise",
+    studentID: "101410368",
+    name: "JEEL PATEL",
+    college: "George Brown College, Toronto"
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" className="App-logo" alt="logo" />
+        <h1>Welcome to {studentInfo.course}</h1>
+        <h2>{studentInfo.labTitle}</h2>
+        <p>{studentInfo.studentID}</p>
+        <p>{studentInfo.name}</p>
+        <p>{studentInfo.college}</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
